8520d379847bb5833f2ad03b590db70292d7277a
Eunchul Song <eunchulsong9@gmail.com>
2021-10-15 10:11:15 +0900

    add application parameter

--- node-rotator/index.js
+++ node-rotator/index.js
@@ -1,18 +1,21 @@
 const { Rotator } = require('./lib');
 
 const { 
-  NODE_SELECTOR, IN_CLUSTER, REPLICAS, NAMESPACE
+  TARGET_NODE_SELECTOR, IN_CLUSTER, REPLICAS, NAMESPACE
 } = process.env;
 
-if(NODE_SELECTOR == undefined) {
-  console.error('NODE_SELECTOR env var must be set');
+if(TARGET_NODE_SELECTOR == undefined) {
+  console.error('TARGET_NODE_SELECTOR env var must be set');
   process.exit(2);
 }
 
-const nodeSelector = Object.fromEntries([NODE_SELECTOR?.split('=')]);
+const nodeSelector = Object.fromEntries([TARGET_NODE_SELECTOR?.split('=')]);
 const inCluster = IN_CLUSTER;
 const replicas = parseInt(REPLICAS) || undefined;
-const namespace = parseInt(NAMESPACE) || 'default';
+const namespace = NAMESPACE || 
+  require('fs').readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/namespace') || 
+  'default';
+
 
 const r = new Rotator({nodeSelector, inCluster, replicas, namespace});
 
